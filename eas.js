ce278f0427da801c1ee0c0ef828c5507da0fa2b9
luccamaidana <99457796+luccamaidana@users.noreply.github.com>
2024-06-27 19:16:23 -0300

    Select the button to change the size of the side when clicking

--- eas.js
+++ eas.js
@@ -1,15 +1,19 @@
 const container = document.querySelector(".container")
+const button = document.querySelector("#changeGrid")
 const divGrid = []
+let size = 16
 
-const createGrid = ()=>{
-    for (let i = 1; i <= 16; i++) {
-        for (let j = 1; j <= 16; j++) {
+const createGrid = (s)=>{
+    container.innerHTML = ``
+    for (let i = 1; i <= s; i++) {
+        for (let j = 1; j <= s; j++) {
             let div = document.createElement("div")
             div.classList.add("div-item")
             container.appendChild(div)
             divGrid.push(div)
         }
     }
+    hoverDiv(divGrid)
 }
 
 const hoverDiv = (div)=>{
@@ -20,5 +24,21 @@ const hoverDiv = (div)=>{
     })
 }
 
-createGrid()
-hoverDiv(divGrid)
+const popUp = ()=>{
+    button.addEventListener("click",()=>{
+        let pixelCant = Number(prompt(`Change the numbers of squares per side for the new grid.\n\nE.g: 16 (min amount) - 100 (max amount)`))
+        if (pixelCant >= 16 && pixelCant <=100){
+            createGrid(pixelCant)
+        } else {
+            alert(`Please, enter a number between 16 and 100`)
+        }
+    })
+}
+
+const main = ()=>{
+    popUp()
+    createGrid(size)
+    
+}
+
+main()
